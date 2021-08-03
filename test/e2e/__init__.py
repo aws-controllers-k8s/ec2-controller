09679054b409fc0b0555541495f31b30a3da2a38
Bryan™️ <crtbry@amazon.com>
2021-08-03 13:54:40 -0500

    add vpc smoke tests

---
+++ test/e2e/__init__.py
@@ -0,0 +1,54 @@
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may
+# not use this file except in compliance with the License. A copy of the
+# License is located at
+#
+# 	 http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+import pytest
+from typing import Dict, Any
+from pathlib import Path
+
+from acktest.k8s import resource
+from acktest.resources import load_resource_file
+
+SERVICE_NAME = "ec2"
+CRD_GROUP = "ec2.services.k8s.aws"
+CRD_VERSION = "v1alpha1"
+
+# PyTest marker for the current service
+service_marker = pytest.mark.service(arg=SERVICE_NAME)
+
+bootstrap_directory = Path(__file__).parent
+resource_directory = Path(__file__).parent / "resources"
+def load_ec2_resource(resource_name: str, additional_replacements: Dict[str, Any] = {}):
+    """ Overrides the default `load_resource_file` to access the specific resources
+    directory for the current service.
+    """
+    return load_resource_file(resource_directory, resource_name, additional_replacements=additional_replacements)
+
+def create_vpc_resource(
+        resource_plural, resource_name, spec_file, replacements, namespace="default"
+):
+    """
+    Wrapper around k8s.load_and_create_resource to create a VPC resource
+    """
+
+    reference, spec, res = resource.load_and_create_resource(
+        resource_directory,
+        CRD_GROUP,
+        CRD_VERSION,
+        resource_plural,
+        resource_name,
+        spec_file,
+        replacements,
+        namespace,
+    )
+
+    return reference, spec, res
---
+++ test/e2e/service_cleanup.py
@@ -0,0 +1,32 @@
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may
+# not use this file except in compliance with the License. A copy of the
+# License is located at
+#
+#	 http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+"""Cleans up the resources created by the ec2 bootstrapping process.
+"""
+
+import logging
+from pathlib import Path
+
+from acktest import resources
+from e2e import bootstrap_directory
+from e2e.bootstrap_resources import TestBootstrapResources
+
+def service_cleanup(config: dict):
+    logging.getLogger().setLevel(logging.INFO)
+
+    resources = TestBootstrapResources(
+        **config
+    )
+
+if __name__ == "__main__":
+    bootstrap_config = resources.read_bootstrap_config(bootstrap_directory)
+    service_cleanup(bootstrap_config) 
