3ba3f310332fdd788a2c283cf02a3dc96092ef8c
Bryan™️ <crtbry@amazon.com>
2021-08-18 13:16:37 -0500

    add terminal codes for subnet

--- pkg/resource/subnet/sdk.go
+++ pkg/resource/subnet/sdk.go
@@ -562,6 +562,18 @@ func (rm *resourceManager) updateConditions(
 // and if the exception indicates that it is a Terminal exception
 // 'Terminal' exception are specified in generator configuration
 func (rm *resourceManager) terminalAWSError(err error) bool {
-	// No terminal_errors specified for this resource in generator config
-	return false
+	if err == nil {
+		return false
+	}
+	awsErr, ok := ackerr.AWSError(err)
+	if !ok {
+		return false
+	}
+	switch awsErr.Code() {
+	case "InvalidVpcID.Malformed",
+		"InvalidVpcID.NotFound":
+		return true
+	default:
+		return false
+	}
 }
