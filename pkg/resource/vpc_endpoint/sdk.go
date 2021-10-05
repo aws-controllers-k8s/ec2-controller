590a32d2857bc3b59ed863f78f99f65ce60c789e
Bryan™️ <crtbry@amazon.com>
2021-10-05 15:14:06 -0500

    add vpc endpoint exceptions

--- pkg/resource/vpc_endpoint/sdk.go
+++ pkg/resource/vpc_endpoint/sdk.go
@@ -695,6 +695,19 @@ func (rm *resourceManager) updateConditions(
 // and if the exception indicates that it is a Terminal exception
 // 'Terminal' exception are specified in generator configuration
 func (rm *resourceManager) terminalAWSError(err error) bool {
-	// No terminal_errors specified for this resource in generator config
-	return false
+	if err == nil {
+		return false
+	}
+	awsErr, ok := ackerr.AWSError(err)
+	if !ok {
+		return false
+	}
+	switch awsErr.Code() {
+	case "InvalidVpcId.Malformed",
+		"InvalidVpcId.NotFound",
+		"InvalidServiceName":
+		return true
+	default:
+		return false
+	}
 }
