27e1df9ab42350c38e9a3d4e0ab81fa0d80029b2
Brandon <wagnerbm@amazon.com>
2021-10-18 18:28:50 -0500

    generate tgw create

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -36,6 +36,7 @@ import (
 	_ "github.com/aws-controllers-k8s/ec2-controller/pkg/resource/internet_gateway"
 	_ "github.com/aws-controllers-k8s/ec2-controller/pkg/resource/route_table"
 	_ "github.com/aws-controllers-k8s/ec2-controller/pkg/resource/subnet"
+	_ "github.com/aws-controllers-k8s/ec2-controller/pkg/resource/transit_gateway"
 	_ "github.com/aws-controllers-k8s/ec2-controller/pkg/resource/vpc"
 	_ "github.com/aws-controllers-k8s/ec2-controller/pkg/resource/vpc_endpoint"
 )
