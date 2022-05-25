3a0ef005f3e362426100a8aded5be370a281fadd
brycahta <crtbry@amazon.com>
2022-05-25 15:07:22 -0500

    late initialize HibernationOptions field for Instance

--- pkg/resource/instance/manager.go
+++ pkg/resource/instance/manager.go
@@ -45,7 +45,7 @@ var (
 // +kubebuilder:rbac:groups=ec2.services.k8s.aws,resources=instances,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=ec2.services.k8s.aws,resources=instances/status,verbs=get;update;patch
 
-var lateInitializeFieldNames = []string{}
+var lateInitializeFieldNames = []string{"HibernationOptions"}
 
 // resourceManager is responsible for providing a consistent way to perform
 // CRUD operations in a backend AWS service API for Book custom resources.
@@ -236,6 +236,10 @@ func (rm *resourceManager) LateInitialize(
 func (rm *resourceManager) incompleteLateInitialization(
 	res acktypes.AWSResource,
 ) bool {
+	ko := rm.concreteResource(res).ko.DeepCopy()
+	if ko.Spec.HibernationOptions == nil {
+		return true
+	}
 	return false
 }
 
@@ -245,7 +249,12 @@ func (rm *resourceManager) lateInitializeFromReadOneOutput(
 	observed acktypes.AWSResource,
 	latest acktypes.AWSResource,
 ) acktypes.AWSResource {
-	return latest
+	observedKo := rm.concreteResource(observed).ko.DeepCopy()
+	latestKo := rm.concreteResource(latest).ko.DeepCopy()
+	if observedKo.Spec.HibernationOptions != nil && latestKo.Spec.HibernationOptions == nil {
+		latestKo.Spec.HibernationOptions = observedKo.Spec.HibernationOptions
+	}
+	return &resource{latestKo}
 }
 
 // IsSynced returns true if the resource is synced.
