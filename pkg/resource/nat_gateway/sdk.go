b5b35579b491b43605a8d976a5e427f9d6a1d2e5
Likitha Vemulapalli <lvemulap@amazon.com>
2022-07-15 09:29:21 -0500

    terminal codes mapping for nat gateway

--- pkg/resource/nat_gateway/sdk.go
+++ pkg/resource/nat_gateway/sdk.go
@@ -530,6 +530,19 @@ func (rm *resourceManager) updateConditions(
 // and if the exception indicates that it is a Terminal exception
 // 'Terminal' exception are specified in generator configuration
 func (rm *resourceManager) terminalAWSError(err error) bool {
-	// No terminal_errors specified for this resource in generator config
-	return false
+	if err == nil {
+		return false
+	}
+	awsErr, ok := ackerr.AWSError(err)
+	if !ok {
+		return false
+	}
+	switch awsErr.Code() {
+	case "InvalidSubnet",
+		"InvalidElasticIpID.Malformed",
+		"MissingParameter":
+		return true
+	default:
+		return false
+	}
 }
