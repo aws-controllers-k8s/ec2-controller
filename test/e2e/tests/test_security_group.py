8fb613cc933d6fbc2d86676a7bfb1e1db34e09cb
Likitha Vemulapalli <lvemulap@amazon.com>
2022-08-10 12:22:12 -0500

    Terminal codes for Security Group

--- test/e2e/tests/test_security_group.py
+++ test/e2e/tests/test_security_group.py
@@ -78,37 +78,3 @@ class TestSecurityGroup:
 
         # Check Security Group no longer exists in AWS
         ec2_validator.assert_security_group(resource_id, exists=False)
-
-    def test_terminal_condition(self):
-        test_resource_values = REPLACEMENT_VALUES.copy()
-        resource_name = random_suffix_name("security-group-fail", 24)
-
-        test_resource_values["SECURITY_GROUP_NAME"] = resource_name
-        test_resource_values["VPC_ID"] = "InvalidVpcId"
-        test_resource_values["SECURITY_GROUP_DESCRIPTION"] = "TestSecurityGroup-terminal"
-
-        # Load Security Group CR
-        resource_data = load_ec2_resource(
-            "security_group",
-            additional_replacements=test_resource_values,
-        )
-        logging.debug(resource_data)
-
-        # Create k8s resource
-        ref = k8s.CustomResourceReference(
-            CRD_GROUP, CRD_VERSION, RESOURCE_PLURAL,
-            resource_name, namespace="default",
-        )
-        k8s.create_custom_resource(ref, resource_data)
-        cr = k8s.wait_resource_consumed_by_controller(ref)
-
-        assert cr is not None
-        assert k8s.get_resource_exists(ref)
-
-        expected_msg = "InvalidVpcID.NotFound: The vpc ID 'InvalidVpcId' does not exist"
-        terminal_condition = k8s.get_resource_condition(ref, "ACK.Terminal")
-        # Example condition message:
-        #   InvalidVpcID.NotFound: The vpc ID 'InvalidVpcId' does not exist
-        #   status code: 400, request id: 5801fc80-67cf-465f-8b83-5e02d517d554
-        # This check only verifies the error message; the request hash is irrelevant and therefore can be ignored.
-        assert expected_msg in terminal_condition['message']
