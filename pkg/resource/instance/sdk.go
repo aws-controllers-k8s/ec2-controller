34fbf0eeedab617fb1790487d44dcd7b6d4cf838
brycahta <crtbry@amazon.com>
2022-10-03 16:32:13 -0500

    instance: set synced iff resource is in a permanent state

--- pkg/resource/instance/sdk.go
+++ pkg/resource/instance/sdk.go
@@ -637,6 +637,18 @@ func (rm *resourceManager) sdkFind(
 		ko.Spec.Tags = r.ko.Spec.Tags
 	}
 
+	// A resource is synced when it has achieved the desired state. However, users
+	// cannot desire a temporary state for an Instance (i.e. pending); therefore,
+	// if the resource is in a temporary state, then set synced to false and reconcile
+	// until a permanent state is achieved.
+	if inTransitoryState(&resource{ko}) {
+		msg := "Instance is in a transitory  state, current status=" + string(*ko.Status.State.Name)
+		ackcondition.SetSynced(&resource{ko}, corev1.ConditionFalse, &msg, nil)
+		return &resource{ko}, nil
+	} else {
+		ackcondition.SetSynced(&resource{ko}, corev1.ConditionTrue, nil, nil)
+	}
+
 	return &resource{ko}, nil
 }
 
--- templates/hooks/instance/sdk_read_many_post_set_output.go.tpl
+++ templates/hooks/instance/sdk_read_many_post_set_output.go.tpl
@@ -5,4 +5,16 @@
 		// then assign resource's tags to maintain tag order
 		ko.Spec.Tags = r.ko.Spec.Tags
 	}
+
+	// A resource is synced when it has achieved the desired state. However, users
+	// cannot desire a temporary state for an Instance (i.e. pending); therefore, 
+	// if the resource is in a temporary state, then set synced to false and reconcile 
+	// until a permanent state is achieved.
+	if inTransitoryState(&resource{ko}) {
+		msg := "Instance is in a transitory  state, current status=" + string(*ko.Status.State.Name)
+		ackcondition.SetSynced(&resource{ko}, corev1.ConditionFalse, &msg, nil)
+		return &resource{ko}, nil
+	} else {
+		ackcondition.SetSynced(&resource{ko}, corev1.ConditionTrue, nil, nil)
+	}
     
