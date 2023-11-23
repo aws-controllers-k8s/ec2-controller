b9daf27a15483f5a3d2f7730e26738395d04e58f
marcdavoli <mdavoli@influxdata.com>
2023-11-23 16:22:02 -0500

    feat: add 2 more parameters to Update op payload

--- pkg/resource/vpc_peering_connection/sdk.go
+++ pkg/resource/vpc_peering_connection/sdk.go
@@ -519,13 +519,25 @@ func (rm *resourceManager) sdkUpdate(
 		return nil, err
 	}
 
-	// Optionally include custom parameters in the 'Update' operation payload before sending
+	// Optionally include parameters in the payload before sending it, if they are in the resource's Spec
 	trueCondition := true
 	if desired.ko.Spec.AccepterPeeringConnectionOptions.AllowDNSResolutionFromRemoteVPC == &trueCondition {
 		input.AccepterPeeringConnectionOptions.AllowDnsResolutionFromRemoteVpc = &trueCondition
 	}
+	if desired.ko.Spec.AccepterPeeringConnectionOptions.AllowEgressFromLocalClassicLinkToRemoteVPC == &trueCondition {
+		input.AccepterPeeringConnectionOptions.AllowEgressFromLocalClassicLinkToRemoteVpc = &trueCondition
+	}
+	if desired.ko.Spec.AccepterPeeringConnectionOptions.AllowEgressFromLocalVPCToRemoteClassicLink == &trueCondition {
+		input.AccepterPeeringConnectionOptions.AllowEgressFromLocalVpcToRemoteClassicLink = &trueCondition
+	}
 	if desired.ko.Spec.RequesterPeeringConnectionOptions.AllowDNSResolutionFromRemoteVPC == &trueCondition {
 		input.RequesterPeeringConnectionOptions.AllowDnsResolutionFromRemoteVpc = &trueCondition
+	}
+	if desired.ko.Spec.RequesterPeeringConnectionOptions.AllowEgressFromLocalClassicLinkToRemoteVPC == &trueCondition {
+		input.RequesterPeeringConnectionOptions.AllowEgressFromLocalClassicLinkToRemoteVpc = &trueCondition
+	}
+	if desired.ko.Spec.RequesterPeeringConnectionOptions.AllowEgressFromLocalVPCToRemoteClassicLink == &trueCondition {
+		input.RequesterPeeringConnectionOptions.AllowEgressFromLocalVpcToRemoteClassicLink = &trueCondition
 	}
 
 	var resp *svcsdk.ModifyVpcPeeringConnectionOptionsOutput
--- templates/hooks/vpc_peering_connection/sdk_update_post_build_request.go.tpl
+++ templates/hooks/vpc_peering_connection/sdk_update_post_build_request.go.tpl
@@ -1,9 +1,21 @@
 	
-	// Optionally include custom parameters in the payload before sending it
+	// Optionally include parameters in the payload before sending it, if they are in the resource's Spec
 	trueCondition := true
 	if desired.ko.Spec.AccepterPeeringConnectionOptions.AllowDNSResolutionFromRemoteVPC == &trueCondition {
 		input.AccepterPeeringConnectionOptions.AllowDnsResolutionFromRemoteVpc = &trueCondition
 	}
+	if desired.ko.Spec.AccepterPeeringConnectionOptions.AllowEgressFromLocalClassicLinkToRemoteVPC == &trueCondition {
+		input.AccepterPeeringConnectionOptions.AllowEgressFromLocalClassicLinkToRemoteVpc = &trueCondition
+	}
+	if desired.ko.Spec.AccepterPeeringConnectionOptions.AllowEgressFromLocalVPCToRemoteClassicLink == &trueCondition {
+		input.AccepterPeeringConnectionOptions.AllowEgressFromLocalVpcToRemoteClassicLink = &trueCondition
+	}
 	if desired.ko.Spec.RequesterPeeringConnectionOptions.AllowDNSResolutionFromRemoteVPC == &trueCondition {
 		input.RequesterPeeringConnectionOptions.AllowDnsResolutionFromRemoteVpc = &trueCondition
+	}
+	if desired.ko.Spec.RequesterPeeringConnectionOptions.AllowEgressFromLocalClassicLinkToRemoteVPC == &trueCondition {
+		input.RequesterPeeringConnectionOptions.AllowEgressFromLocalClassicLinkToRemoteVpc = &trueCondition
+	}
+	if desired.ko.Spec.RequesterPeeringConnectionOptions.AllowEgressFromLocalVPCToRemoteClassicLink == &trueCondition {
+		input.RequesterPeeringConnectionOptions.AllowEgressFromLocalVpcToRemoteClassicLink = &trueCondition
 	}
