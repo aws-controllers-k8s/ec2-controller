a786837a1a515645842db6baac2da8d2da838852
marcdavoli <mdavoli@influxdata.com>
2023-11-29 14:08:42 -0500

    chore: remove unneeded hook

--- pkg/resource/vpc/hooks.go
+++ pkg/resource/vpc/hooks.go
@@ -331,10 +331,66 @@ func applyPrimaryCIDRBlockInCreateRequest(r *resource,
 // '.spec.rejectVpcPeeringRequestsFromVpcRef'
 // to either Accept or Reject incoming VPC Peering Connection requests that have the current VPC
 // as the 'Accepter VPC' and that have the status 'Pending Acceptance'
-func respondToPendingVpcPeeringConnectionRequests(
+func (rm *resourceManager) respondToPendingVpcPeeringConnectionRequests(
 	ctx context.Context,
 	desired *resource,
 ) {
+
+	// if r.ko.Spec.AcceptVPCPeeringRequestsFromVPCIDs != nil ||
+	// 	r.ko.Spec.AcceptVPCPeeringRequestsFromVPCRefs != nil ||
+	// 	r.ko.Spec.RejectVPCPeeringRequestsFromVPCIDs != nil ||
+	// 	r.ko.Spec.RejectVPCPeeringRequestsFromVPCRefs != nil {
+
+	// 	// Create an EC2 client using the AWS SDK Go v2
+	// 	cfg, err := config.LoadDefaultConfig(ctx)
+	// 	if err != nil {
+	// 		fmt.Println("Error loading AWS SDK config:", err)
+	// 		return
+	// 	}
+	// 	// Create an EC2 client
+	// 	ec2Client := ec2.NewFromConfig(cfg)
+
+	// 	// Describe all VPC Peering connections that are Pending Acceptance
+	// 	input := *svcsdk.DescribeVpcPeeringConnectionsInput{
+	// 		Filters: []svcsdk.Filter{
+	// 			{
+	// 				Name:   aws.String("status-code"),
+	// 				Values: []string{"pending-acceptance"},
+	// 			},
+	// 		},
+	// 	}
+	// 	resp, err := client.DescribeVpcPeeringConnections(ctx, input)
+	// 	if err != nil {
+	// 		fmt.Println("Error describing VPC peering connections:", err)
+	// 		return
+	// 	}
+	// 	peeringConnections = resp.VpcPeeringConnections
+
+	// 	// Iterate through each VPC Peering connection
+	// 	for _, peeringConnection := range peeringConnections {
+	// 		// Check if the peerVpcId is our VPC
+	// 		if peeringConnection.AccepterVPCInfo.VpcID == desired.ko.Spec.AcceptVPCPeeringRequestsFromVPCID {
+	// 			// Check if the VpcId is in our allow-list of VPCs
+	// 			if isVPCInWhitelist(peeringConnection.RequesterVPCInfo.VpcID) {
+	// 				// Accept the VPC Peering Request
+	// 				err := acceptVPCPeeringRequest(ec2Client, peeringConnection.VpcPeeringConnectionId)
+	// 				if err != nil {
+	// 					return err
+	// 				}
+	// 			}
+	// 		}
+	// 		if peeringConnection.AccepterVPCInfo.VpcID == desired.ko.Spec.RejectVPCPeeringRequestsFromVPCID {
+	// 			// Check if the VpcId is in our block-list of VPCs
+	// 			if isVPCInWhitelist(peeringConnection.RequesterVPCInfo.VpcID) {
+	// 				// Reject the VPC Peering Request
+	// 				err := rejectVPCPeeringRequest(ec2Client, peeringConnection.VpcPeeringConnectionId)
+	// 				if err != nil {
+	// 					return err
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	//}
 
 	// log.errors
 
