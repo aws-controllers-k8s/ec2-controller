9c15021686e5fe37beb8d3d5179d5c7d9fc700c4
marcdavoli <mdavoli@influxdata.com>
2023-12-06 08:28:43 -0500

    test: add debug logging

--- pkg/resource/vpc_peering_connection/sdk.go
+++ pkg/resource/vpc_peering_connection/sdk.go
@@ -242,13 +242,13 @@ func (rm *resourceManager) sdkFind(
 	rm.setStatusDefaults(ko)
 
 	// Hack to artificially trigger detection by delta.DifferentAt("Spec.AcceptRequest")
-	rlog.Debug("Hack to artificially trigger detection", "resource", r)
+	rlog.Debug("Hack to artificially trigger detection", "resource", r, "ko", ko)
 	if isVPCPeeringConnectionPendingAcceptance(r) {
-		rlog.Debug("Setting VPC Peering Connection spec.acceptRequest to false", "Resource", r)
-		r.ko.Spec.AcceptRequest = aws.Bool(false)
-	} else if isVPCPeeringConnectionActive(r) || isVPCPeeringConnectionProvisioning(r) {
 		rlog.Debug("Setting VPC Peering Connection spec.acceptRequest to true", "Resource", r)
 		r.ko.Spec.AcceptRequest = aws.Bool(true)
+	} else if isVPCPeeringConnectionActive(r) || isVPCPeeringConnectionProvisioning(r) {
+		rlog.Debug("Setting VPC Peering Connection spec.acceptRequest to false", "Resource", r)
+		r.ko.Spec.AcceptRequest = aws.Bool(false)
 	}
 
 	return &resource{ko}, nil
