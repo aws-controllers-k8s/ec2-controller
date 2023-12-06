7b82a32d5fe6d866a3c23d3b8c14209500e810b9
marcdavoli <mdavoli@influxdata.com>
2023-12-05 21:19:38 -0500

    chore: add debug logs

--- pkg/resource/vpc_peering_connection/sdk.go
+++ pkg/resource/vpc_peering_connection/sdk.go
@@ -243,8 +243,10 @@ func (rm *resourceManager) sdkFind(
 
 	// Hack to artificially trigger detection by delta.DifferentAt("Spec.AcceptRequest")
 	if isVPCPeeringConnectionPendingAcceptance(r) {
+		rlog.Debug("Setting VPC Peering Connection spec.acceptRequest to false", "Resource", r)
 		r.ko.Spec.AcceptRequest = aws.Bool(false)
 	} else if isVPCPeeringConnectionActive(r) || isVPCPeeringConnectionProvisioning(r) {
+		rlog.Debug("Setting VPC Peering Connection spec.acceptRequest to true", "Resource", r)
 		r.ko.Spec.AcceptRequest = aws.Bool(true)
 	}
 
--- templates/hooks/vpc_peering_connection/sdk_read_many_post_set_output.go.tpl
+++ templates/hooks/vpc_peering_connection/sdk_read_many_post_set_output.go.tpl
@@ -1,7 +1,9 @@
 
    // Hack to artificially trigger detection by delta.DifferentAt("Spec.AcceptRequest")
 	if isVPCPeeringConnectionPendingAcceptance(r) {
+		rlog.Debug("Setting VPC Peering Connection spec.acceptRequest to false", "Resource", r)
 		r.ko.Spec.AcceptRequest = aws.Bool(false)
 	} else if isVPCPeeringConnectionActive(r) || isVPCPeeringConnectionProvisioning(r) {
+		rlog.Debug("Setting VPC Peering Connection spec.acceptRequest to true", "Resource", r)
 		r.ko.Spec.AcceptRequest = aws.Bool(true)
 	}
