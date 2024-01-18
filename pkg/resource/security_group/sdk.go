2d9805527813e391ec7ab7835fff27baec2c1de1
vflaux <38909103+vflaux@users.noreply.github.com>
2024-01-18 12:03:08 +0100

    Fix AWS Security Group leakage at creation when an error occures (#174)
    fix aws-controllers-k8s/community#1990
    
    Description of changes: 
    Change the hook `security_group/sdk_create_post_set_output.go.tpl` to always return the resource even if an error occurred. At this point the Security Group exists and we should keep track of it.
    
    By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

--- pkg/resource/security_group/sdk.go
+++ pkg/resource/security_group/sdk.go
@@ -226,18 +226,18 @@ func (rm *resourceManager) sdkCreate(
 
 	// Delete the default egress rule
 	if err = rm.deleteDefaultSecurityGroupRule(ctx, &resource{ko}); err != nil {
-		return nil, err
+		return &resource{ko}, err
 	}
 
 	if err = rm.syncSGRules(ctx, &resource{ko}, nil); err != nil {
-		return nil, err
+		return &resource{ko}, err
 	}
 
 	// A ReadOne call for SecurityGroup Rules (NOT SecurityGroups)
 	// is made to refresh Status.Rules with the recently-updated
 	// data from the above `sync` call
 	if rules, err := rm.getRules(ctx, &resource{ko}); err != nil {
-		return nil, err
+		return &resource{ko}, err
 	} else {
 		ko.Status.Rules = rules
 	}
