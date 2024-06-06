cec98ed921c2f3c412c85de5dd4256890243ddc5
Junfeng Wu <wjf3121@hotmail.com>
2024-06-06 10:40:55 +0800

    fix: do not update synced state on non pending state

--- pkg/resource/vpc_endpoint/sdk.go
+++ pkg/resource/vpc_endpoint/sdk.go
@@ -258,8 +258,6 @@ func (rm *resourceManager) sdkFind(
 		// Setting resource synced condition to false will trigger a requeue of
 		// the resource. No need to return a requeue error here.
 		ackcondition.SetSynced(&resource{ko}, corev1.ConditionFalse, nil, nil)
-	} else {
-		ackcondition.SetSynced(&resource{ko}, corev1.ConditionTrue, nil, nil)
 	}
 
 	return &resource{ko}, nil
