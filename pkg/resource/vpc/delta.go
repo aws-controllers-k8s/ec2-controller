2b040a460444e46d482fbcff59d2e272759d7d67
ci-robot <ci-robot@k8s.io>
2024-10-10 04:01:12 +0000

    Update to ACK runtime `v0.39.0`, code-generator `v0.39.1`

--- pkg/resource/vpc/delta.go
+++ pkg/resource/vpc/delta.go
@@ -42,6 +42,7 @@ func newResourceDelta(
 		delta.Add("", a, b)
 		return delta
 	}
+	customPreCompare(delta, a, b)
 
 	if ackcompare.HasNilDifference(a.ko.Spec.AmazonProvidedIPv6CIDRBlock, b.ko.Spec.AmazonProvidedIPv6CIDRBlock) {
 		delta.Add("Spec.AmazonProvidedIPv6CIDRBlock", a.ko.Spec.AmazonProvidedIPv6CIDRBlock, b.ko.Spec.AmazonProvidedIPv6CIDRBlock)
