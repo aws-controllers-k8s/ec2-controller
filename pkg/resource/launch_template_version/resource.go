cc0a8ce49cc30cb0057f3ddaa33e7b563e244609
rkurduka <rkurduka@amazon.com>
2025-01-05 12:41:25 +0530

    generate controller with latest code-generator after rebase

--- pkg/resource/launch_template_version/resource.go
+++ pkg/resource/launch_template_version/resource.go
@@ -98,6 +98,22 @@ func (r *resource) SetIdentifiers(identifier *ackv1alpha1.AWSIdentifiers) error 
 	return nil
 }
 
+// PopulateResourceFromAnnotation populates the fields passed from adoption annotation
+func (r *resource) PopulateResourceFromAnnotation(fields map[string]string) error {
+	tmp, ok := fields["launchTemplateID"]
+	if !ok {
+		return ackerrors.MissingNameIdentifier
+	}
+	r.ko.Spec.LaunchTemplateID = &tmp
+
+	f3, f3ok := fields["launchTemplateName"]
+	if f3ok {
+		r.ko.Spec.LaunchTemplateName = &f3
+	}
+
+	return nil
+}
+
 // DeepCopy will return a copy of the resource
 func (r *resource) DeepCopy() acktypes.AWSResource {
 	koCopy := r.ko.DeepCopy()
