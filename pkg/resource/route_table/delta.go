9bd0e21b5d28986a121e29e1da7017560968a0be
Gereon Frey <gfrey@adobe.com>
2025-02-20 11:38:00 +0100

    Update generated code using code-generator v0.43.2

--- pkg/resource/route_table/delta.go
+++ pkg/resource/route_table/delta.go
@@ -44,13 +44,6 @@ func newResourceDelta(
 	}
 	customPreCompare(delta, a, b)
 
-	if len(a.ko.Spec.Routes) != len(b.ko.Spec.Routes) {
-		delta.Add("Spec.Routes", a.ko.Spec.Routes, b.ko.Spec.Routes)
-	} else if len(a.ko.Spec.Routes) > 0 {
-		if !reflect.DeepEqual(a.ko.Spec.Routes, b.ko.Spec.Routes) {
-			delta.Add("Spec.Routes", a.ko.Spec.Routes, b.ko.Spec.Routes)
-		}
-	}
 	if !ackcompare.MapStringStringEqual(ToACKTags(a.ko.Spec.Tags), ToACKTags(b.ko.Spec.Tags)) {
 		delta.Add("Spec.Tags", a.ko.Spec.Tags, b.ko.Spec.Tags)
 	}
