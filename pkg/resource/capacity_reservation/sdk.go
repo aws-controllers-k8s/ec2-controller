0e472295542d0a6a709837143837a553cbaac79e
Fiza Shaikh <fiza.shaikh@workday.com>
2025-05-26 16:53:25 +1200

    fix_capacity_reservation_adoption: Fix updating reservations if additionalInfo field is passed

--- pkg/resource/capacity_reservation/sdk.go
+++ pkg/resource/capacity_reservation/sdk.go
@@ -609,6 +609,15 @@ func (rm *resourceManager) sdkUpdate(
 		return nil, err
 	}
 
+	// If additionalInfo field is being updated, other fields cannot be modified simultaneously,
+	// hence we reset them or else we run into InvalidParameterCombination error
+	if input.AdditionalInfo != nil {
+		input.InstanceCount = nil
+		input.EndDate = nil
+		input.EndDateType = ""
+		input.InstanceMatchCriteria = ""
+	}
+
 	var resp *svcsdk.ModifyCapacityReservationOutput
 	_ = resp
 	resp, err = rm.sdkapi.ModifyCapacityReservation(ctx, input)
