9c1ab5c9ed8434e40098d244049c4dc78cf4f687
Trevor Knottnerus <tknott@amazon.com>
2025-07-07 23:22:30 -0700

    Add tests for adopting VPC endpoint service

--- test/e2e/service_bootstrap.py
+++ test/e2e/service_bootstrap.py
@@ -19,6 +19,7 @@ from acktest.bootstrapping import Resources, BootstrapFailureException
 from acktest.bootstrapping.elbv2 import NetworkLoadBalancer
 from acktest.bootstrapping.vpc import VPC
 from acktest.bootstrapping.vpc import TransitGateway
+from acktest.bootstrapping.vpc_endpoint_service import VpcEndpointServiceConfiguration
 from acktest.bootstrapping.s3 import Bucket
 from e2e import bootstrap_directory
 from e2e.bootstrap_resources import BootstrapResources
@@ -37,7 +38,8 @@ def service_bootstrap() -> Resources:
         ),
         NetworkLoadBalancer=NetworkLoadBalancer("e2e-vpc-ep-service-test"),
         AdoptedVPC=VPC(name_prefix="e2e-adopted-vpc", num_public_subnet=1, num_private_subnet=0),
-        TestTransitGateway=TransitGateway()
+        TestTransitGateway=TransitGateway(),
+        AdoptedVpcEndpointService=VpcEndpointServiceConfiguration(name_prefix="e2e-adopted-vpc-es"),
     )
 
     try:
