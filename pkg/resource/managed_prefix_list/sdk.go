9396c33ab1c503f8ce4ffb050b03967b0f1863bc
Alexandru Dobre <doalexan@adobe.com>
2025-10-30 11:15:30 +0200

    Fix filter logic

--- pkg/resource/managed_prefix_list/sdk.go
+++ pkg/resource/managed_prefix_list/sdk.go
@@ -75,6 +75,26 @@ func (rm *resourceManager) sdkFind(
 	if err != nil {
 		return nil, err
 	}
+	// If we have a PrefixListID, use it to filter the describe call
+	if r.ko.Status.PrefixListID != nil {
+		input.PrefixListIds = []string{*r.ko.Status.PrefixListID}
+	} else if r.ko.Spec.PrefixListName != nil {
+		// If we don't have an ID yet, filter by both owner ID and prefix list name
+		// This prevents matching against AWS-managed prefix lists
+		// and other user-owned prefix lists with different names
+		filters := []svcsdktypes.Filter{
+			{
+				Name:   aws.String("owner-id"),
+				Values: []string{string(rm.awsAccountID)},
+			},
+			{
+				Name:   aws.String("prefix-list-name"),
+				Values: []string{*r.ko.Spec.PrefixListName},
+			},
+		}
+		input.Filters = filters
+	}
+
 	var resp *svcsdk.DescribeManagedPrefixListsOutput
 	resp, err = rm.sdkapi.DescribeManagedPrefixLists(ctx, input)
 	rm.metrics.RecordAPICall("READ_MANY", "DescribeManagedPrefixLists", err)
