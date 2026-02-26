3c3c98c11c8e2cb227fcf9d137039ab6d923a8b7
Mickaël Carl <contact@carlm.org>
2026-02-26 00:08:00 +0000

    fix: don't delete vpcendpointserviceconfiguration resources when API deletion fails (#310)
    Fixes https://github.com/aws-controllers-k8s/community/issues/2791.
    
    Description of changes:
    
    When deleting a vpcendpointserviceconfiguration, it could be that the actual VPC endpoint service still has connections. Currently this would fail silently: the resource would be deleted on the k8s side, but the VPC endpoint service would simply remain, and no error would be emitted. This fixes that by adding an additional hook that inspects the response, and check for errors in the Unsuccessful array, returning the first one in case there is any.
    We could return all errors, but that doesn't feel particularly worth doing, since the other error would apply to other VPCES but we only ever send one request from this operator.
    
    By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

--- pkg/resource/vpc_endpoint_service_configuration/sdk.go
+++ pkg/resource/vpc_endpoint_service_configuration/sdk.go
@@ -528,6 +528,14 @@ func (rm *resourceManager) sdkDelete(
 	_ = resp
 	resp, err = rm.sdkapi.DeleteVpcEndpointServiceConfigurations(ctx, input)
 	rm.metrics.RecordAPICall("DELETE", "DeleteVpcEndpointServiceConfigurations", err)
+	if err == nil && resp != nil && len(resp.Unsuccessful) > 0 {
+		errMsg := "failed to delete VPC Endpoint Service Configuration"
+		if resp.Unsuccessful[0].Error != nil && resp.Unsuccessful[0].Error.Message != nil {
+			errMsg = errMsg + ": " + *resp.Unsuccessful[0].Error.Message
+		}
+		err = fmt.Errorf("%s", errMsg)
+	}
+
 	return nil, err
 }
 
